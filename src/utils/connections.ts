d2dff657e0e109b38d47df13f2fcaf5f7e272eba
niitroxendioxide <niitroxendioxide@gmail.com>
2025-05-30 17:54:19 -0300

    compu de abajo ip

--- src/utils/connections.ts
+++ src/utils/connections.ts
@@ -23,7 +23,7 @@ class WebSocketManager {
     }
 
     private initialize() {
-        this.ws = new WebSocket('ws://192.168.50.14:8080');
+        this.ws = new WebSocket('ws://192.168.30.147:8080');
 
         this.ws.onopen = () => {
             console.log('Connected!');
